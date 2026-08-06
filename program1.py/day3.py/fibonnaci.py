aa0dcf63fc6bb6fc7ba0cceb9c6271ac93fe409d
amitramapur74-star <amitramapur74@gmail.com>
2026-08-06 11:46:34 +0530

    fibo commt

--- program1.py/day3.py/fibonnaci.py
+++ program1.py/day3.py/fibonnaci.py
@@ -1,10 +1,7 @@
-def fibonacci():
+def square_table():
     n = int(input("Enter number of terms: "))
 
-    a = 0
-    b = 1
+    for i in range(1, n + 1):
+        print(f"{i} * {i} = {i * i}")
 
-    for i in range(n):
-        print(a, end=" ")
-        a, b = b, a + b
-fibonacci()
+square_table()
